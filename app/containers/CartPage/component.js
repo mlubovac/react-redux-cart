2512c4d08ca561f371ffc28bcebe2571e5481c74
mlubovac <volimcajke@yahoo.com>
2016-10-22 22:02:02 -0700

    added table

--- app/containers/CartPage/component.js
+++ app/containers/CartPage/component.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import CartTableRow from './CartTableRow';
 
 class CartPage extends Component {
     render() {
         return (
-            <h1>Cart Page</h1>
+            <div>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Item</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                            <th>Total</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.props.cartItems.map(cartItem => <CartTableRow key={cartItem.guid} cartItem={cartItem} />)
+                        }                    
+                    </tbody>
+                </table>
+            </div>
         );
     }
 };
