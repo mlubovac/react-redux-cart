b55fe67c6694f11fd6e192dc4504504882256e13
mlubovac <volimcajke@yahoo.com>
2016-10-22 22:03:05 -0700

    added methods and cases

--- app/containers/CartPage/reducer.js
+++ app/containers/CartPage/reducer.js
@@ -1,10 +1,54 @@
-const cartReducer = (state = {}, action) => {
+const cartReducer = (state = [], action) => {
+    
     const { type, payload } = action;
 
     switch (type) {
+
+        case "ADD_ITEM_TO_CART":
+            return addItemToCart(state, payload);
+
+        case "REMOVE_ITEM_FROM_CART":
+            return removeItemFromCart(state, payload);            
+
+        case "SET_CART_ITEM_QUANTITY":
+            return setItemQuantity(state, payload);;
+
         default:
             return state;
     }
 };
 
 export default cartReducer;
+
+const isItemInCart = (state, payload) => {
+    return state.filter((item) => item.guid === payload.guid).length; 
+};
+
+const removeItemFromCart = (state, payload) => {
+    return state.filter((item) => item.guid !== payload.guid)
+};
+
+const incrementItemQuantity = (state, payload) => {    
+    return state.map(item => {
+        if (item.guid === payload.guid) {
+            item.quantity += 1;
+        }
+        return item;
+    });    
+};
+
+const setItemQuantity = (state, payload) => {    
+    return state.map(item => {
+        if (item.guid === payload.guid) {
+            item.quantity = payload.quantity;
+        }
+        return item;
+    });    
+};
+
+const addItemToCart = (state, payload) =>  {
+    if (!isItemInCart(state, payload)) {
+        return [...state, payload]; 
+    }
+    return incrementItemQuantity(state, payload);
+};
