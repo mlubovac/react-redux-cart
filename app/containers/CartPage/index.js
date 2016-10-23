63dbacce150d28fe01e7b645ac1e19df6996228f
mlubovac <volimcajke@yahoo.com>
2016-10-23 12:27:41 -0700

    added property, method

--- app/containers/CartPage/index.js
+++ app/containers/CartPage/index.js
@@ -1,9 +1,18 @@
 import { connect } from 'react-redux';
 import CartPage from './component';
 
+
+const getCartTotal =  (cart) => {
+    return cart.reduce((prev, curr) => {
+        return prev += (curr.price * curr.quantity)
+    }, 0).toFixed(2);
+};
+
+
 const mapStateToProps = (state, ownProps) => {
     return {
-        cartItems: state.cart
+        cart: state.cart,
+        cartTotal: getCartTotal(state.cart)
     };
 };
 
