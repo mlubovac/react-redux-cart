8f606b22a7105b1eade084eddc8745da92c28e83
mlubovac <volimcajke@yahoo.com>
2016-10-23 12:28:28 -0700

    added column styles

--- app/containers/CartPage/CartTableRow.js
+++ app/containers/CartPage/CartTableRow.js
@@ -2,24 +2,21 @@ import React, { Component } from 'react';
 import CartItemQuantityInput from './../CartItemQuantityInput';
 import RemoveCartItemButton from './../RemoveCartItemButton';
 
-class CartTableRow extends Component {
-    render() {
-        const { name, guid, quantity, price } = this.props.cartItem;
-        const lineTotal = (quantity * price).toFixed(2);
-        return (
-            <tr>
-                <td>{name}</td>
-                <td style={{width: "10rem"}}>
-                    <CartItemQuantityInput guid={guid} quantity={quantity} />
-                </td>
-                <td>{price}</td>
-                <td>{lineTotal}</td>
-                <td>
-                    <RemoveCartItemButton guid={guid} />
-                </td>
-            </tr>
-        );
-    }
-}
+const CartTableRow = ({ name, guid, quantity, price }) => {
+    const lineTotal = (quantity * price).toFixed(2);
+    return (
+        <tr>
+            <td>{name}</td>
+            <td style={{textAlign: "right"}}>{price}</td>                
+            <td>
+                <CartItemQuantityInput guid={guid} quantity={quantity} />
+            </td>
+            <td style={{textAlign: "right"}}>{lineTotal}</td>
+            <td style={{textAlign: "center"}}>
+                <RemoveCartItemButton guid={guid} />
+            </td>
+        </tr>
+    );
+};
 
 export default CartTableRow;
